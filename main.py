170d45718bc066271a1cf4298f40911dd0dd2f85
OriginalElijah3 <chocolate_krazer@yahoo.com>
2024-12-10 11:33:02 -0500

    added player file and draw player method.

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 import pygame
 from constants import * 
+from player import Player
 
 pygame.init()
 
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    # print("Starting asteroids!")
+    # print(f"Screen width: {SCREEN_WIDTH}")
+    # print(f"Screen height: {SCREEN_HEIGHT}")
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
+    game_time = pygame.time.Clock()
+
+    dt = 0
+
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+
+    player = Player(x, y)
+
     running = True
     
     while running:
@@ -16,7 +27,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+
+        player.draw(screen)
+
         pygame.display.flip()
+        
+        dt = game_time.tick(60)/1000
 
 if __name__ == "__main__":
     main()
